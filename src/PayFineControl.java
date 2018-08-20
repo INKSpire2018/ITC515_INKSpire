3324e2dafd60f0af877cd5d096a2ca9e189b8fb9
chamath <chamathherath@yahoo.com>
2018-08-20 17:02:30 +1000

    library changed to Library_<string name=txtCredits><a href=http://www.vogella.com/tutorials/AndroidListView/article.html>Click to view website</a>></string>

--- src/PayFineControl.java
+++ src/PayFineControl.java
@@ -5,7 +5,8 @@ public class PayFineControl {
 	private enum CONTROL_STATE { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
 	private CONTROL_STATE state;
 	
-	private library library;
+	//A class name cannot be started from simple letter.library changed to Library
+	private Library library;
 	private member member;;
 
 
