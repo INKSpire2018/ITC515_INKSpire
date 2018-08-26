8cddb414333137135ce53a1e45381dbaa97fca31
HashanSukitha <sukitharox@yahoo.com>
2018-08-26 10:45:40 +0530

    indentation Arranged
    indentation Arranged

--- src/ReturnBookControl.java
+++ src/ReturnBookControl.java
@@ -17,8 +17,10 @@ public class ReturnBookControl
 	}
 	
 	
-	public void setUI(ReturnBookUI ui) {
-		if (!state.equals(CONTROL_STATE.INITIALISED)) {
+	public void setUI(ReturnBookUI ui) 
+	{
+		if (!state.equals(CONTROL_STATE.INITIALISED)) 
+		{
 			// Change the wording in the Exception -> to start with a capital letter "Cannot"
 			throw new RuntimeException("ReturnBookControl: Cannot call setUI except in INITIALISED state");
 		}	
@@ -28,31 +30,38 @@ public class ReturnBookControl
 	}
 
 
-	public void bookScanned(int bookId) {
-		if (!state.equals(CONTROL_STATE.READY)) {
+	public void bookScanned(int bookId) 
+	{
+		if (!state.equals(CONTROL_STATE.READY)) 
+		{
 			// Change the wording in the Exception -> to start with a capital letter "Cannot"
 			throw new RuntimeException("ReturnBookControl: Cannot call bookScanned except in READY state");
 		}	
 		book currentBook = library.Book(bookId);
 		
-		if (currentBook == null) {
+		if (currentBook == null) 
+		{
 			ui.display("Invalid Book Id");
 			return;
 		}
-		if (!currentBook.On_loan()) {
+		if (!currentBook.On_loan()) 
+		{
 			ui.display("Book has not been borrowed");
 			return;
 		}		
 		currentLoan = library.getLoanByBookId(bookId);	
 		double overDueFine = 0.0;
-		if (currentLoan.isOverDue()) {
+		
+		if (currentLoan.isOverDue()) 
+		{
 			overDueFine = library.calculateOverDueFine(currentLoan);
 		}
 		ui.display("Inspecting");
 		ui.display(currentBook.toString());
 		ui.display(currentLoan.toString());
 		
-		if (currentLoan.isOverDue()) {
+		if (currentLoan.isOverDue()) 
+		{
 			ui.display(String.format("\nOverdue fine : $%.2f", overDueFine));
 		}
 		ui.setState(ReturnBookUI.UI_STATE.INSPECTING);
@@ -60,8 +69,10 @@ public class ReturnBookControl
 	}
 
 
-	public void scanningComplete() {
-		if (!state.equals(CONTROL_STATE.READY)) {
+	public void scanningComplete() 
+	{
+		if (!state.equals(CONTROL_STATE.READY)) 
+		{
 			// Change the wording in the Exception -> to start with a capital letter "Cannot"
 			throw new RuntimeException("ReturnBookControl: Cannot call scanningComplete except in READY state");
 		}	
@@ -69,8 +80,10 @@ public class ReturnBookControl
 	}
 
 
-	public void dischargeLoan(boolean isDamaged) {
-		if (!state.equals(CONTROL_STATE.INSPECTING)) {
+	public void dischargeLoan(boolean isDamaged) 
+	{
+		if (!state.equals(CONTROL_STATE.INSPECTING))
+		{
 			// Change the wording in the Exception -> to start with a capital letter "Cannot"
 			throw new RuntimeException("ReturnBookControl: Cannot call dischargeLoan except in INSPECTING state");
 		}	
